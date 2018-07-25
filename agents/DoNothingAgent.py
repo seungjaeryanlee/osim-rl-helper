6c21c5ec9cb954c18c1061f4baf10ba5c5893a6d
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-07-25 10:16:37 -1000

    Updated DoNothingAgent.__init__()

--- agents/DoNothingAgent.py
+++ agents/DoNothingAgent.py
@@ -5,8 +5,8 @@ class DoNothingAgent(Agent):
     """
     An agent that chooses NOOP action at every timestep.
     """
-    def __init__(self, env):
-        self.action = [0] * 19
+    def __init__(self, observation_space, action_space):
+        self.action = [0] * action_space.shape[0]
 
     def act(self, observation):
         return self.action
